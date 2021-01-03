6d6c88d3fbb34454b9887fac54b1d0e4704e3109
Sravani-Sravani <74003159+Sravani-Sravani@users.noreply.github.com>
2021-01-03 19:35:57 +0530

    Update program6.js

--- program6.js
+++ program6.js
@@ -2,3 +2,7 @@
 var name;
 name=window.prompt("enter Name","Sravani");
 document.writeln("<h3>"+name+"</h3>");
+var a;
+a=parseInt(prompt("Enter value",0))
+document.writeln("<h3>"+a+"</h3>")
+window.alert(a);
