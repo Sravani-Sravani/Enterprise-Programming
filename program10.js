edd03ef8347b098fedeb33d21827b01c6c23a37c
Sravani-Sravani <74003159+Sravani-Sravani@users.noreply.github.com>
2021-01-03 19:49:18 +0530

    Update program10.js

--- program10.js
+++ program10.js
@@ -5,3 +5,13 @@ if(n%2==0)
 	alert(n+ "is Even Number");
 else
 	alert(n+ "is Odd Number");
+
+
+var n,m;
+n=parseInt(prompt("Enter Value",0))
+m=parseInt(prompt("Enter Value",0))
+
+if(n>m)
+	alert(n+ " is Big");
+else
+	alert(m+ " is Big");
