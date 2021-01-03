207f30217173718b09fa1c8308144d2c1948738a
Sravani-Sravani <74003159+Sravani-Sravani@users.noreply.github.com>
2021-01-03 19:20:48 +0530

    Create program3.js

---
+++ program3.js
@@ -0,0 +1,13 @@
+//3 type of operator
+		var name="Sravani"
+		var num=40
+		var avg=30.67
+		var booleanvalue=true
+		var num1
+		var num2=0
+		document.writeln(typeof(name)+"<br>")
+		document.writeln(typeof(num)+"<br>")
+		document.writeln(typeof(avg)+"<br>")
+		document.writeln(typeof(booleanvalue)+"<br>")
+		document.writeln(typeof(num1)+"<br>")
+		document.writeln(typeof(num2)+"<br>")
