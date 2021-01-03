3cf44522ce1722d6059b19ae60f84b7f1b2dcb88
Sravani-Sravani <74003159+Sravani-Sravani@users.noreply.github.com>
2021-01-03 19:25:07 +0530

    Create program4.js

---
+++ program4.js
@@ -0,0 +1,13 @@
+//4 implicit type conversion
+  var a,b,c,d,e;
+  a=12*5;
+  b="Covid"+10;
+  c=12*"5";
+  d=12*"hello";
+  e=19*"klu10"
+
+  document.writeln(a+"<br>");
+  document.writeln(b+"<br>");
+  document.writeln(c+"<br>");
+  document.writeln(d+"<br>");  //NaN-not a number
+document.writeln(e+"<br>");  
