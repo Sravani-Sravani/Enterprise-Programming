241d2dfdf8793621c06e5e05e507c0c413f031bb
Sravani-Sravani <74003159+Sravani-Sravani@users.noreply.github.com>
2021-01-03 19:27:31 +0530

    Create program5.js

---
+++ program5.js
@@ -0,0 +1,6 @@
+//5 explicit type conversion
+ document.writeln(parseInt("covid19")+"<br>");
+document.writeln(parseFloat("12.25abc")+"<br>");
+document.writeln(parseInt("19covid")+"<br>");
+document.writeln(parseInt("12.25")+"<br>");
+document.writeln(parseFloat("abc12.25")+"<br>");
