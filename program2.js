2f668a5b6ec632b02951c548b1a59eb7bfdbd32f
Sravani-Sravani <74003159+Sravani-Sravani@users.noreply.github.com>
2021-01-03 19:18:25 +0530

    Create program2.js

---
+++ program2.js
@@ -0,0 +1,7 @@
+//2 add 2 numbers
+        var name="covid"
+        var num = 100
+        var sum = 150.23
+        var booleanvalue = true
+        var num1
+        document.writeln(typeof(name)+"<br>");
