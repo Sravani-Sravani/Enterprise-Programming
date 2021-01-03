ccf6e99f3fbd01911310435e05aef15b0095de04
Sravani-Sravani <74003159+Sravani-Sravani@users.noreply.github.com>
2021-01-03 19:51:32 +0530

    Create program12.js

---
+++ program12.js
@@ -0,0 +1,11 @@
+//12 else if ladder
+        var a,b,c;
+    a=parseInt(prompt("Enter first value",0))
+    b=parseInt(prompt("Enter second value",0))
+    c=parseInt(prompt("Enter third value",0))
+    if (a>=b&&a>=c)
+          alert(a+" is big")
+        else if (b>=a&&b>=c)
+          alert(b+" is big")
+        else
+            alert(c+" is big")
